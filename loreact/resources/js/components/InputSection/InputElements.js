b01ee0ad28cabace3947e70eb951c43c7118923a
Darlin Noula <darnoul20@gmail.com>
2021-01-27 11:35:21 +0100

    ajout des cartes

--- loreact/resources/js/components/InputSection/InputElements.js
+++ loreact/resources/js/components/InputSection/InputElements.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-
+import {Link} from 'react-router-dom'
 
 export const InputContainer = styled.div`
     display:flex;
@@ -12,11 +12,26 @@ export const InputContainer = styled.div`
     height: auto;
     overflow:hidden;
     border-radius:20px;
-    height: 700px;
+    height: auto;
     top:-120px;
     box-shadow:8px 8px 10px gray;
     text-align:center;
     line-height:60px;
     font-family:Nunito
 
+`
+
+export const InputWrapper = styled.div`
+    display:flex;
+    flex-wrap:wrap;
+    margin:40px;
+    justify-content:space-between
+
+`
+
+export const CardLink = styled(Link)`
+    width:auto;
+    height:auto;
+    text-decoration:none;
+    color:gray
 `
--- loreact/resources/js/components/InputSection/index.js
+++ loreact/resources/js/components/InputSection/index.js
@@ -1,40 +1,49 @@
 import React from 'react'
-import {InputContainer} from './InputElements'
+import {InputContainer,InputWrapper,CardLink} from './InputElements'
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FaSearch from 'react-icons/fa'
 import {Accordion,Card} from 'react-bootstrap'
-
+import liyep from '../assets/liyep.png'
+import simbcoin from '../assets/simbcoin.png'
+import limarket from '../assets/limarket.png'
 
 
 export const InputSection = () => {
     return (
         <InputContainer>
             <Form>
-                <FormGroup>
+                {/* <FormGroup>
                      <Input type="text" name="email" id="exampleEmail" placeholder="Decivez nous votre probleme" style={{marginTop:"40px", width:"500px", marginLeft:"90px"}}></Input>
 
-                </FormGroup>
-                <h2 style={{marginTop:"80px"}}> Suggestions de problèmes </h2>
-                <Accordion defaultActiveKey="0">
-                    <Card>
-                        <Card.Header>
-                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                            Liyeplimall
-                        </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey="0">
-                            <Card.Body>Hello! I'm the body</Card.Body>
-                            
-
-                        </Accordion.Collapse>
-                        <Accordion.Collapse eventKey="0">
-                            <Card.Body>Hello! I'm the body</Card.Body>
-                            
-
-                        </Accordion.Collapse>
+                </FormGroup> */}
+                <h2 style={{marginTop:"80px"}}> Choisissez une categorie </h2>
+                <InputWrapper>
+                    <CardLink>
+                    <Card style={{ width: '18rem'}} className="border-rounded">
+                        <Card.Img variant="top" src={liyep} />
+                        <Card.Body>
+                        <Card.Title>Liyeplimall</Card.Title>
+                        
+                        </Card.Body>
                     </Card>
-                </Accordion>
+                    </CardLink>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={simbcoin}  />
+                        <Card.Body>
+                        <Card.Title>Simbcoin</Card.Title>
+                        
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem',marginLeft: "140px" }}>
+                        <Card.Img variant="top" src={limarket} />
+                        <Card.Body>
+                        <Card.Title>Liyeplimall</Card.Title>
+                        
+                        </Card.Body>
+                    </Card>
+                </InputWrapper>
             </Form>
         </InputContainer>
     )
